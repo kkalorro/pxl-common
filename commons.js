0c36d2107fce57fd37d6412f37bcfc41a7d99355
kkalorro <kkalorro@gmail.com>
2020-12-17 11:50:46 -0800

    Cleaned up unused code and comments.  Refactored existing code.

--- commons.js
+++ commons.js
@@ -1,13 +1,3 @@
-// Version
-const version = '0.0.0.1';
-
-// Container to host all functions
-const methods = {};
-
-methods.test = function() {
-    console.log(`common.js loaded successfully. Version: ${version}`);
-}
-
 // -------------------
 // ACCOUNT LOGIN
 // Get a string.
@@ -16,68 +6,77 @@ methods.test = function() {
 // Run next step.
 // -------------------
 
+// Methods need to return object back
+// Need a function that reads objects
+
 //////////////////
 // Declarations //
 //////////////////
+
+// Version
+const version = '0.0.0.2';
+
+// Container to host all functions
+const methods = {};
 
 // [Temp] Stored data
 const storedData = [
     {
         id: 1,
-        name: 'first'
+        name: 'AAA'
     },
     {
         id: 2,
-        name: 'second'
+        name: 'BBB'
     },
     {
         id: 3,
-        test: 'new'
+        test: 'AAA'
     }
 ];
-
-// ACCOUNT LOGIN
-// Get a string.
-
 
 ///////////////
 // Functions //
 ///////////////
 
+// Test to make sure file loaded successfully.
+methods.test = () => console.log(`common.js loaded successfully. Version: ${version}`);
+
 // Search array array for specified criteria and return matches
-// Future: adjust array to object and array
 methods.findObjects = function(array, property, value) {
     // Return all results, no criteria
-    function returnAll() {
-        console.log('Running returnAll().');
-        // Go through preexisting stored data
-        for (let i = 0; i < array.length; i++) {
-            // Return containing objects for Property matches
-            console.log(array[i]);
-        }
+    function matchesAll() {
+        for (i in array) console.log(array[i]);     
     }
 
     // Return all results matching Property
-    function returnByProperty(property) {
-        console.log('Running returnByProperty().');
-        // Go through preexisting stored data
+    function matchesProperty(property) {
+        // Go through array
         for (let i = 0; i < array.length; i++) {
-            // On every keypair
+            // Go through each keypair in array and return object containing strings matching Property
             for (keypair in array[i]) {
-                // Return containing objects for Property matches
                 (keypair === property) ? console.log(array[i]) : false;
             }
         }
     }
 
+    // Return all results matching Value
+    function matchesValue(value) {
+        // Go through array
+        for (let i = 0; i < array.length; i++) {
+            // Go through each keypair in array and return object containing strings matching Property
+            for (keypair in array[i]) {
+                (array[i][keypair] === value) ? console.log(array[i]) : false;
+            }
+        }
+    }
+
     // Return all results with matching Property and Value
-    function returnByKeypair(property, value) {
-        console.log('Running returnByKeypair().');
+    function matchesKeypair(property, value) {
         // Go through preexisting stored data
         for (let i = 0; i < array.length; i++) {
-            // On every keypair
+            // Go through each keypair in array and return object containing strings matching Property and Value
             for (keypair in array[i]) {
-                // Return containing objects for Property matches
                 ((keypair === property) && (array[i][keypair] === value)) ? console.log(array[i]) : false;
             }
         }
@@ -85,39 +84,32 @@ methods.findObjects = function(array, property, value) {
 
     // Error and arguement handling
     if (!property && value) {
-        console.log('ERROR: Property must be included when searching for a Value.');
-    // Return all if no keypair value is declared
+        // console.log('ERROR: Property must be included when searching for a Value.');
+        matchesValue(value);
     } else if (!property && !value) {
-        console.log('Outputting all results.');
-        returnAll();
+        matchesAll();
     } else if (property && value) {
-        console.log('Outputting all results matching Property and Value.');
-        returnByKeypair(property, value);
+        matchesKeypair(property, value);
     } else if (property && !value) {
-        console.log('Outputting all results matching Property name.');
-        returnByProperty(property);
+        matchesProperty(property);
     }
-        
 }
-
-// CRUD.
-// Run next step.
 
 /////////////
 // Runtime //
 /////////////
 
 // Test functions
-console.log('TESTING: Full search.');
+console.log('TESTING: Search by Property and Value.');
 methods.findObjects(storedData, 'id', 3);
 
-console.log('TESTING: Minimal search.');
+console.log('TESTING: Search by no criteria.');
 methods.findObjects(storedData, '', null);
 
-console.log('TESTING: Missing Property.');
-methods.findObjects(storedData, '', 'second');
+console.log('TESTING: Search by Value.');
+methods.findObjects(storedData, '', 'AAA');
 
-console.log('TESTING: Missing Value.')
+console.log('TESTING: Search by Property.')
 methods.findObjects(storedData, 'name', null);
 
 // Export methods for other files to use
